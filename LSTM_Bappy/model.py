e771383f5490c5e5a47ea2b593790b8194fc4ad5
vam-sin <vamsinallapareddy@gmail.com>
2019-06-28 13:37:18 +0530

    LSTM to image mask

--- LSTM_Bappy/model.py
+++ LSTM_Bappy/model.py
@@ -6,10 +6,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import mixem
 from keras.models import Sequential
+from keras.backend import squeeze
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import LSTM, Dense, Flatten, Conv2DTranspose, Conv2D, MaxPooling2D, TimeDistributed
+from keras.layers import LSTM, Dense, Reshape, Flatten, Conv2DTranspose, Conv2D, MaxPooling2D, TimeDistributed
 import glob
 import cv2
+from keras.optimizers import SGD
+
+opt = SGD(lr=0.005, momentum=0.9)
 
 # Dimensions
 # Output: (429976, 3)
@@ -21,13 +25,14 @@ train_datagen = ImageDataGenerator()
 
 training_X = train_datagen.flow_from_directory('../COVERAGE/Forged_Im',
                                                  target_size = (374, 324),
-                                                 batch_size = 4,
+                                                 batch_size = 1,
                                                  class_mode = None)
 
 training_Y = train_datagen.flow_from_directory('../COVERAGE/Forged_Im_M',
-                                            target_size = (),
-                                            batch_size = 4,
+                                            target_size = (8, 8),
+                                            batch_size = 1,
                                             class_mode = None)
+
 dataset = zip(training_X, training_Y)
 
 print('Dataset Prepared')
@@ -65,15 +70,39 @@ print('Dataset Prepared')
 # ])
 
 # Stacked LSTM
-model = Sequential()
-model.add(TimeDistributed(Conv2D(30,kernel_size = (5,5),
-    strides=(1,1),padding='valid',activation='relu'), input_shape=(374, 324, 3)))
-model.add(LSTM(64,activation='tanh',return_sequences=True))
-model.add(LSTM(64,return_sequences=True, activation='tanh'))
-model.add(LSTM(64, activation='tanh'))
+def LSTM_Model():
+    model = Sequential()
+    model.add(Conv2D(30, kernel_size=(5,5), strides=(1,1), activation='relu', input_shape=(374, 324, 3)))
+    model.add(Conv2D(30, kernel_size=(5,5), strides=(1,1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(16, kernel_size=(3,3), strides=(1,1), activation='relu'))
+    model.add(Conv2D(16, kernel_size=(3,3), strides=(1,1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(16, kernel_size=(3,3), strides=(1,1), activation='relu'))
+    model.add(Conv2D(16, kernel_size=(3,3), strides=(1,1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(16, kernel_size=(3,3), strides=(1,1), activation='relu'))
+    model.add(Conv2D(16, kernel_size=(3,3), strides=(1,1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(16, kernel_size=(3,3), strides=(1,1), activation='relu'))
+    model.add(Conv2D(16, kernel_size=(3,3), strides=(1,1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Reshape((1,672)))
+    model.add(LSTM(64, activation='tanh', return_sequences=True))
+    model.add(LSTM(64, activation='tanh', return_sequences=True))
+    model.add(LSTM(64*3, activation='tanh', return_sequences=True))
+    model.add(Reshape((8,8,3)))
+    model.compile(optimizer = opt, loss = 'mse', metrics=['accuracy'])
+    model.summary()
 
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics=['accuracy'])
-model.summary()
+    return model
+
+classifier = LSTM_Model()
+
+checkpoint = ModelCheckpoint('model_lstm.h5', monitor='acc', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint]
+
+classifier.fit_generator(dataset, steps_per_epoch=100, epochs=20, verbose=1, callbacks=callbacks_list)
 
 # # Training on Forged
 # for i in range(len(images_forged)):
